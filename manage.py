35c70829fba0c700cf25fb07c71f103ce9d58e34
nadejdentoshev <nadejdentoshev@gmail.com>
2025-06-10 15:55:09 +0300

    Write multiline string

--- manage.py
+++ manage.py
@@ -11,9 +11,9 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exception:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            """Couldn't import Django. Are you sure it's installed and 
+            available on your PYTHONPATH environment variable? Did you 
+            forget to activate a virtual environment?"""
         ) from exception
     
     execute_from_command_line(sys.argv)
